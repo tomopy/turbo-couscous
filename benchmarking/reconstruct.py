49d3d1abfeedb6b4f5afe422a8513bfaa68ee4ba
Daniel Ching <carterbox@users.noreply.github.com>
2019-06-24 14:34:23 -0500

    API: Update how tests are chosen

--- benchmarking/reconstruct.py
+++ benchmarking/reconstruct.py
@@ -60,76 +60,133 @@ logger = logging.getLogger(__name__)
 def main(phantom, num_iter, max_iter, output_dir, ncore, parameters):
     """Reconstruct data using TomoPy.
 
-    Chooses which algorithms to run based on the name of the conda environment.
-    Expects three environments: astra, 1.1, and 1.5 which has the tomopy
-    API of those versions.
+    Automatically chooses which algorithms to run based the TomoPy
+    version number and whether third-party modules are importable.
+    Alternatively, a string representing a python list of dictionaries
+    of parameters can be provided to run a custom subset of tests.
     """
     data = np.load(os.path.join(output_dir, phantom, 'simulated_data.npz'))
     dynamic_range = np.max(data['original'])
     if parameters is not None:
         parameters = ast.literal_eval(parameters)
-    elif 'astra' in os.environ['CONDA_PREFIX']:
-        import astra
-        parameters = [
-            {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA'}},
-            {'algorithm': tomopy.astra, 'num_iter': num_iter, 'options': {'proj_type': 'cuda', 'method': 'SIRT_CUDA', 'num_iter': num_iter}},
-            {'algorithm': tomopy.astra, 'num_iter': num_iter, 'options': {'proj_type': 'cuda', 'method': 'CGLS_CUDA', 'num_iter': num_iter}},
-            {'algorithm': tomopy.astra, 'num_iter': num_iter, 'options': {'proj_type': 'cuda', 'method': 'EM_CUDA', 'num_iter': num_iter}},
-            {'algorithm': tomopy.astra, 'num_iter': num_iter, 'options': {'proj_type': 'cuda', 'method': 'SART_CUDA', 'num_iter': num_iter}},
-        ]
-    elif '1.1' in os.environ['CONDA_PREFIX']:
-        parameters = [
-            {'algorithm': 'gridrec', 'filter_name': 'none'},
-            {'algorithm': 'gridrec', 'filter_name': 'butterworth'},
-            {'algorithm': 'gridrec', 'filter_name': 'cosine'},
-            {'algorithm': 'gridrec', 'filter_name': 'hamming'},
-            {'algorithm': 'gridrec', 'filter_name': 'hann'},
-            {'algorithm': 'gridrec', 'filter_name': 'parzen'},
-            {'algorithm': 'gridrec', 'filter_name': 'ramlak'},
-            {'algorithm': 'gridrec', 'filter_name': 'shepp'},
-            {'algorithm': 'fbp'},
-            {'algorithm': 'art', 'num_iter': num_iter},
-            {'algorithm': 'bart', 'num_iter': num_iter},
-            {'algorithm': 'mlem', 'num_iter': num_iter},
-            {'algorithm': 'osem', 'num_iter': num_iter},
-            {'algorithm': 'ospml_hybrid', 'num_iter': num_iter},
-            {'algorithm': 'ospml_quad', 'num_iter': num_iter},
-            {'algorithm': 'pml_hybrid', 'num_iter': num_iter},
-            {'algorithm': 'pml_quad', 'num_iter': num_iter},
-            {'algorithm': 'sirt', 'num_iter': num_iter},
-        ]
-    elif '1.5' in os.environ['CONDA_PREFIX']:
-        parameters = [
-            {'algorithm': 'gridrec', 'filter_name': 'none'},
-            {'algorithm': 'gridrec', 'filter_name': 'butterworth'},
-            {'algorithm': 'gridrec', 'filter_name': 'cosine'},
-            {'algorithm': 'gridrec', 'filter_name': 'hamming'},
-            {'algorithm': 'gridrec', 'filter_name': 'hann'},
-            {'algorithm': 'gridrec', 'filter_name': 'parzen'},
-            {'algorithm': 'gridrec', 'filter_name': 'ramlak'},
-            {'algorithm': 'gridrec', 'filter_name': 'shepp'},
-            {'algorithm': 'grad', 'num_iter': num_iter, 'reg_par': -1},
-            # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'NN'},
-            # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'LINEAR'},
-            # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'CUBIC'},
-            # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'NN'},
-            # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'LINEAR'},
-            # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'CUBIC'},
-            {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'NN'},
-            # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'LINEAR'},
-            # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'CUBIC'},
-            # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'NN'},
-            # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'LINEAR'},
-            # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'CUBIC'},
-            {'algorithm': 'tv', 'num_iter': num_iter},
-        ]
     else:
-        raise ValueError("Test environment not recognized.")
+        parameters = [
+            {'algorithm': 'gridrec', 'filter_name': 'none'},
+            {'algorithm': 'gridrec', 'filter_name': 'butterworth'},
+            {'algorithm': 'gridrec', 'filter_name': 'cosine'},
+            {'algorithm': 'gridrec', 'filter_name': 'hamming'},
+            {'algorithm': 'gridrec', 'filter_name': 'hann'},
+            {'algorithm': 'gridrec', 'filter_name': 'parzen'},
+            {'algorithm': 'gridrec', 'filter_name': 'ramlak'},
+            {'algorithm': 'gridrec', 'filter_name': 'shepp'},
+        ]
+        if float(tomopy.__version__[:3]) < 1.5:
+            parameters += [
+                {'algorithm': 'fbp'},
+                {'algorithm': 'art', 'num_iter': num_iter},
+                {'algorithm': 'bart', 'num_iter': num_iter},
+                {'algorithm': 'mlem', 'num_iter': num_iter},
+                {'algorithm': 'osem', 'num_iter': num_iter},
+                {'algorithm': 'ospml_hybrid', 'num_iter': num_iter},
+                {'algorithm': 'ospml_quad', 'num_iter': num_iter},
+                {'algorithm': 'pml_hybrid', 'num_iter': num_iter},
+                {'algorithm': 'pml_quad', 'num_iter': num_iter},
+                {'algorithm': 'sirt', 'num_iter': num_iter},
+            ]
+        if float(tomopy.__version__[:3]) >= 1.5:
+            parameters += [
+                {'algorithm': 'gridrec', 'filter_name': 'none'},
+                {'algorithm': 'gridrec', 'filter_name': 'butterworth'},
+                {'algorithm': 'gridrec', 'filter_name': 'cosine'},
+                {'algorithm': 'gridrec', 'filter_name': 'hamming'},
+                {'algorithm': 'gridrec', 'filter_name': 'hann'},
+                {'algorithm': 'gridrec', 'filter_name': 'parzen'},
+                {'algorithm': 'gridrec', 'filter_name': 'ramlak'},
+                {'algorithm': 'gridrec', 'filter_name': 'shepp'},
+                {'algorithm': 'grad', 'num_iter': num_iter, 'reg_par': -1},
+                # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'NN'},
+                # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'LINEAR'},
+                # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'CUBIC'},
+                # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'NN'},
+                # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'LINEAR'},
+                # {'algorithm': 'mlem', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'CUBIC'},
+                {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'NN'},
+                # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'LINEAR'},
+                # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'gpu', 'interpolation': 'CUBIC'},
+                # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'NN'},
+                # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'LINEAR'},
+                # {'algorithm': 'sirt', 'num_iter': num_iter, 'accelerated': True, 'device': 'cpu', 'interpolation': 'CUBIC'},
+                {'algorithm': 'tv', 'num_iter': num_iter},
+            ]
+        try:
+            import astra
+            parameters += [
+                {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'ram-lak'}},
+                # FIXME: Not confident that these filter parameters are being passed to astra
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'shepp-logan'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'cosine'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'hamming'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'hann'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'none'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'tukey'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'lanczos'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'triangular'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'gaussian'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'barlett-hann'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'blackman'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'nuttall'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'blackman-harris'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'blackman-nuttall'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'flat-top'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'kaiser'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'parzen'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'projection'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'sinogram'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'rprojection'}},
+                # {'algorithm': tomopy.astra, 'options': {'proj_type': 'cuda', 'method': 'FBP_CUDA', 'FilterType': 'rsinogram'}},
+                {'algorithm': tomopy.astra, 'num_iter': num_iter, 'options':
+                    {'proj_type': 'cuda', 'method': 'SIRT_CUDA', 'num_iter': num_iter}},
+                {'algorithm': tomopy.astra, 'num_iter': num_iter, 'options':
+                    {'proj_type': 'cuda', 'method': 'CGLS_CUDA', 'num_iter': num_iter}},
+                {'algorithm': tomopy.astra, 'num_iter': num_iter, 'options':
+                    {'proj_type': 'cuda', 'method': 'EM_CUDA', 'num_iter': num_iter}},
+                {'algorithm': tomopy.astra, 'num_iter': num_iter, 'options':
+                    {'proj_type': 'cuda', 'method': 'SART_CUDA', 'num_iter': num_iter}},
+            ]
+        except ImportError:
+            tomopy.astra = None
+        try:
+            import lprec
+            parameters += [
+                {'algorithm': tomopy.lprec, 'lpmethod': 'fbp', 'filter_name': 'parzen'},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'fbp', 'filter_name': 'ramp'},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'fbp', 'filter_name': 'shepp-logan'},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'fbp', 'filter_name': 'cosine'},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'fbp', 'filter_name': 'cosine2'},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'fbp', 'filter_name': 'hamming'},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'fbp', 'filter_name': 'hann'},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'cg', 'num_iter': num_iter},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'em', 'num_iter': num_iter},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'grad', 'num_iter': num_iter},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'tv', 'num_iter': num_iter},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'tve', 'num_iter': num_iter},
+                {'algorithm': tomopy.lprec, 'lpmethod': 'tvl1', 'num_iter': num_iter},
+            ]
+        except ImportError:
+            tomopy.lprec = None
     for params in parameters:  # yapf: disable
         if 'device' in params and params['device'] == 'gpu':
             params.update({'ncore': 1, 'pool_size': ncore})
         elif 'device' in params and params['device'] == 'cpu':
             params.update({'ncore': 2, 'pool_size': 8})
+        elif params['algorithm'] is tomopy.lprec:
+            params.update({'ncore': 1})
+        elif params['algorithm'] is tomopy.astra:
+            params.update({'ncore': 1})
+            try:
+                params['filter_name'] = params['options']['FilterType']
+            except KeyError:
+                pass
         else:
             params.update({'ncore': ncore})
         reconstruct(
@@ -179,7 +236,9 @@ def reconstruct(
     """
     logger.info('{}'.format(params))
     if params['algorithm'] is tomopy.astra:
-        algorithm = params['options']['method']
+        algorithm = 'astra-' + params['options']['method'].lower()
+    elif params['algorithm'] is tomopy.lprec:
+        algorithm = 'lprec-' + params['lpmethod']
     else:
         algorithm = params['algorithm']
     base_path = os.path.join(output_dir, phantom, algorithm)
