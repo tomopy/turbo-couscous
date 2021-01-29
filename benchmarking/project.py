893d63312b8b1b482cf28c7f03949aba1852cbcf
Dreycen Foiles <foilesdreycen@gmail.com>
2021-01-29 13:08:36 -0600

    Changed output path to current date

--- benchmarking/project.py
+++ benchmarking/project.py
@@ -9,6 +9,8 @@ import click
 import tomopy
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import date
+
 
 logger = logging.getLogger(__name__)
 
@@ -74,7 +76,7 @@ logger = logging.getLogger(__name__)
 @click.option(
     '-o',
     '--output-dir',
-    default=os.path.join('tomopy-benchmarks/local', tomopy.__version__),
+    default=os.path.join('tomopy-benchmarks/'+str(date.today()), tomopy.__version__),
     help='Folder to put data inside.',
     type=click.Path(exists=False),
 )
