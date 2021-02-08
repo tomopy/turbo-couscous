9d601e11f64ded5caf13c2b6bd8e3228f25dca45
Dreycen Foiles <foilesdreycen@gmail.com>
2021-02-08 14:59:14 -0600

    Created shell scripts to run benchmarking process

---
+++ run-benchmarking-gpu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+currentDate=`date +%F`
+outputDir=tomopy.github.io/$currentDate
+
+python -Om benchmarking.project \
+  --poisson 5 \
+  --trials 4 \
+  --width 800 \
+  --num-angles 32 \
+  --phantom peppers \
+  --output-dir $outputDir \
+
+python -Om benchmarking.reconstruct \
+  --ncore 16 \
+  --num-iter 5 \
+  --max-iter 50 \
+  --phantom peppers \
+  --parameters "[{'algorithm': 'mlem', 'num_iter': 5, 'accelerated': True, 'device': 'gpu', 'interpolation': 'NN'}, {'algorithm': 'mlem', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'LINEAR'},{'algorithm': 'mlem', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'CUBIC'},{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'NN'},{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'LINEAR'},{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'CUBIC'}]" \
+  --output-dir $outputDir \
+
+python -Om benchmarking.summarize \
+  --phantom peppers \
+  --trials 32 \
+  --output-dir $outputDir\
