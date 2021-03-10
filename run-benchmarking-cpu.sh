26c511114eea58b428a625e73edaeabd964d3200
Dreycen Foiles <foilesdreycen@gmail.com>
2021-03-09 21:20:19 -0600

    Complete benchmarking cycle

--- run-benchmarking-cpu.sh
+++ run-benchmarking-cpu.sh
@@ -4,21 +4,18 @@ currentDate=`date +%F`
 outputDir=tomopy.github.io/$currentDate/cpu
 
 python -Om benchmarking.project \
-  --poisson 5 \
-  --trials 4 \
-  --width 800 \
-  --num-angles 32 \
+  --poisson 500 \
+  --trials 32 \
+  --width 1446 \
+  --num-angles 1500 \
   --phantom peppers \
-  --output-dir $outputDir \
 
 python -Om benchmarking.reconstruct \
   --ncore 16 \
-  --max-iter 30 \
+  --num-iter 5 \
+  --max-iter 50 \
   --phantom peppers \
-  --parameters "[{'algorithm': 'ospml_hybrid'},{'algorithm': 'osem'}]" \
-  --output-dir $outputDir \
 
 python -Om benchmarking.summarize \
   --phantom peppers \
   --trials 32 \
-  --output-dir $outputDir \
--- run-benchmarking-gpu.sh
+++ run-benchmarking-gpu.sh
@@ -5,9 +5,9 @@ outputDir=tomopy.github.io/$currentDate/gpu
 
 python -Om benchmarking.project \
   --poisson 5 \
-  --trials 4 \
-  --width 800 \
-  --num-angles 32 \
+  --trials 32 \
+  --width 1446 \
+  --num-angles 1500 \
   --phantom peppers \
   --output-dir $outputDir \
 
@@ -15,8 +15,7 @@ python -Om benchmarking.reconstruct \
   --ncore 16 \
   --max-iter 30 \
   --phantom peppers \
-  --parameters "[{'algorithm': 'sirt', 'accelerated': True, 'device': 'gpu', 'interpolation': 'NN'}, {'algorithm': 'sirt', 'accelerated':
-                True, 'device': 'gpu', 'interpolation': 'LINEAR'}]" \
+  --parameters "[{'algorithm': 'mlem', 'num_iter': 5, 'accelerated': True, 'device': 'gpu', 'interpolation': 'NN'}, {'algorithm': 'mlem', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'LINEAR'},{'algorithm': 'mlem', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'CUBIC'},{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'NN'},{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'LINEAR'},{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'CUBIC'}]" \
   --output-dir $outputDir \
 
 python -Om benchmarking.summarize \
