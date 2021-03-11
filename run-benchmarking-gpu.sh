ecff1fc196c011a462120a07f70a0cab9de0fdf5
Dreycen Foiles <foilesdreycen@gmail.com>
2021-03-11 13:39:31 -0600

    Fixed num iter option

--- run-benchmarking-gpu.sh
+++ run-benchmarking-gpu.sh
@@ -4,9 +4,9 @@ currentDate=`date +%F`
 outputDir=tomopy.github.io/$currentDate/gpu
 
 python -Om benchmarking.project \
-  --poisson 5 \
+  --poisson 500 \
   --trials 32 \
-  --width 1448 \
+  --width 1446 \
   --num-angles 1500 \
   --phantom peppers \
   --output-dir $outputDir \
@@ -15,7 +15,7 @@ python -Om benchmarking.reconstruct \
   --ncore 16 \
   --max-iter 50 \
   --phantom peppers \
-  --parameters "[{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'NN'},{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'LINEAR'},{'algorithm': 'sirt', 'num_iter': 5, 'accelerated':True, 'device': 'gpu', 'interpolation': 'CUBIC'}]" \
+  --parameters "[{'algorithm': 'sirt', 'accelerated':True, 'device': 'gpu', 'interpolation': 'NN'},{'algorithm': 'sirt', 'accelerated':True, 'device': 'gpu', 'interpolation': 'LINEAR'},{'algorithm': 'sirt', 'accelerated':True, 'device': 'gpu', 'interpolation': 'CUBIC'}]" \
   --output-dir $outputDir \
 
 python -Om benchmarking.summarize \
