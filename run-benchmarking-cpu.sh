a0ac762604f2b970e11a224592ccb01abbf87029
Dreycen Foiles <foilesdreycen@gmail.com>
2021-03-11 13:39:24 -0600

    Fixed output directory

--- run-benchmarking-cpu.sh
+++ run-benchmarking-cpu.sh
@@ -6,15 +6,18 @@ outputDir=tomopy.github.io/$currentDate/cpu
 python -Om benchmarking.project \
   --poisson 500 \
   --trials 32 \
-  --width 1448 \
+  --width 1446 \
   --num-angles 1500 \
   --phantom peppers \
+  --output-dir $outputDir \
 
 python -Om benchmarking.reconstruct \
   --ncore 16 \
   --max-iter 50 \
   --phantom peppers \
+  --output-dir $outputDir \
 
 python -Om benchmarking.summarize \
   --phantom peppers \
   --trials 32 \
+  --output-dir $outputDir \
